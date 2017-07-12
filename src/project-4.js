ac43a2b851236508943fbfd4106f019b2beb33a6
Antonio Melendez <antoniomelendez@Antonios-MacBook-Pro.local>
2017-07-11 20:46:41 -0600

    All tests pass for project-4

--- src/project-4.js
+++ src/project-4.js
@@ -1,30 +1,55 @@
 // Do not change any of the function names
 
-const multiplyArguments = () => {
+const multiplyArguments = (...args) => {
   // use the arguments keyword to multiply all of the arguments together and return the product
   // if no arguments are passed in return 0
   // if one argument is passed in just return it
+  if (args.length === 1) {
+    return args[0];
+  }
+  if (args.length === 0) {
+    return 0;
+  }
+  let product = args[0];
+  for (let i = 1; i < args.length; i++) {
+    product *= args[i];
+  }
+  return product;
 };
 
 const invokeCallback = (cb) => {
   // invoke cb
+  cb();
 };
 
 const sumArray = (numbers, cb) => {
   // sum up all of the integers in the numbers array
   // pass the result to cb
   // no return is necessary
+  let total = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    total += numbers[i];
+  }
+  cb(total);
 };
 
 const forEach = (arr, cb) => {
   // iterate over arr and pass its values to cb one by one
   // hint: you will be invoking cb multiple times (once for each value in the array)
+  for (let i = 0; i < arr.length; i++) {
+    cb(arr[i]);
+  }
 };
 
 const map = (arr, cb) => {
   // create a new array
   // iterate over each value in arr, pass it to cb, then place the value returned from cb into the new arr
   // the new array should be the same length as the array argument
+  const newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    newArr[i] = cb(arr[i]);
+  }
+  return newArr;
 };
 
 const getUserConstructor = () => {
@@ -34,18 +59,37 @@ const getUserConstructor = () => {
   // the constructor should have a method 'sayHi' on its prototype that returns the string 'Hello, my name is {{name}}'
   // {{name}} should be the name set on each instance
   // return the constructor
+  class User {
+    constructor(options) {
+      this.username = options.username;
+      this.name = options.name;
+      this.email = options.email;
+      this.password = options.password;
+    }
+  }
+  User.prototype.sayHi = () => {
+    return `Hello, my name is ${this.name}`;
+  };
+  return User;
 };
 
 const addPrototypeMethod = (Constructor) => {
   // add a method to the constructor's prototype
   // the method should be called 'sayHi' and should return the string 'Hello World!'
+  Constructor.prototype.sayHi = () => {
+    return 'Hello World!';
+  };
 };
 
 const addReverseString = () => {
-  // add a method to the string constructor's prototype that returns a reversed copy of the string
-  // name this method reverse
-  // hint:
-  // you will need to use 'this' inside of reverse
+    // add a method to the string constructor's prototype that returns a reversed copy of the string
+    // name this method reverse
+    // hint:
+    // you will need to use 'this' inside of reverse
+    // eslint-disable-next-line no-extend-native
+  String.prototype.reverse = function reverse() {
+    return this.split('').reverse().join('');
+  };
 };
 
 const nFactorial = (n) => {
@@ -53,6 +97,10 @@ const nFactorial = (n) => {
   // solve this recursively
   // example:
   // the factorial of 3 is 6 (3 * 2 * 1)
+  if (n === 1) {
+    return 1;
+  }
+  return n * nFactorial(n - 1);
 };
 
 const cacheFunction = (cb) => {
@@ -67,6 +115,16 @@ const cacheFunction = (cb) => {
   // if the function you return is invoked with 5 it would pass 5 to cb(5) and return 25
   // if the function you return is invoked again with 5 it will look on an object in the closure scope
   // and return 25 directly and will not invoke cb again
+  const cache = {};
+  return function inner(arg) {
+    let value = 0;
+    if (arg in cache) {
+      return cache[arg];
+    }
+    value = cb(arg);
+    cache[arg] = value;
+    return cache[arg];
+  };
 };
 
 
